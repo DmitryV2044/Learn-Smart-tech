1b96316c537c370d401a7a2127c874aaffab08c9
DmitryV2044 <dvovchinskiy@inbox.ru>
2021-10-26 19:23:29 +0500

    решение нескольких задач из книги

--- programm1.cpp
+++ programm1.cpp
@@ -2,8 +2,58 @@
 #include <cstdlib>
 using namespace std;
 
+void Solution1(){
+    int numAmount;
+    cout<<"До скольки считаем? ";
+    cin>>numAmount;
+
+    for (int i = 0; i < numAmount; i++)
+    {
+        if(i%4 == 3){
+            cout<<"число с остатком 3 при делении на 4: "<<i<<endl;
+        }
+    }
+}
+
+void Fibonacci(){
+    int numAmount;
+    cout<<"До скольки считаем? ";
+    cin>>numAmount;
+
+    long currentNumber = 1;
+    long previousNumber = 0;
+
+    long res;
+
+    cout<<"числа фибоначи: ";
+    for (long i = 0; i < numAmount; i++)
+    {
+        res = currentNumber + previousNumber;
+
+        previousNumber = currentNumber;
+
+        currentNumber = res;
+
+        cout<<res<<", ";
+    }
+
+    cout<<"end"<<endl;
+}
+
 int main(){
-
-
+    int whichTask;
+    cout<<"какую задачу решаем? ";
+    cin>>whichTask;
+    switch (whichTask)
+    {
+    case 1:
+        Solution1();
+        break;
+    case 2:
+        Fibonacci();
+        break;
+    default:
+        break;
+    }
     return 0;
 }
